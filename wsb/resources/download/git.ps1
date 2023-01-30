1b3b4946c5ee8de09b932e639d10ad549bb6c0b4
Coding for fun <peter@reuteras.net>
2023-01-30 20:34:36 +0100

    Add git repositories

--- wsb/resources/download/git.ps1
+++ wsb/resources/download/git.ps1
@@ -3,8 +3,11 @@
 New-Item -ItemType Directory -Force -Path tools\downloads\git
 cd tools\downloads\git
 
-$repourls = "https://github.com/volexity/threat-intel.git", `
-    "https://github.com/Neo23x0/signature-base"
+$repourls = `
+    "https://github.com/keydet89/Events-Ripper.git", `
+    "https://github.com/Neo23x0/signature-base.git", `
+    "https://github.com/SigmaHQ/sigma.git", `
+    "https://github.com/volexity/threat-intel.git"
 
 foreach ($repourl in $repourls) {
     $repo = Write-Output $repourl | % { $_ -replace "^.*/" } | % { $_ -replace "\..*$" }
