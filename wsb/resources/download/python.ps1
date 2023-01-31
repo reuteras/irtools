f75fd0d41fcc632496a88784fbf4a0ee3d570c71
Coding for fun <peter@reuteras.net>
2023-01-31 10:40:42 +0100

    Change to one package per line.

--- wsb/resources/download/python.ps1
+++ wsb/resources/download/python.ps1
@@ -7,11 +7,42 @@ python -m pip install -U pip
 python -m pip install pip2pi
 
 pip2pi ./tools/downloads/pip `
-    chepy[extras] dnslib docx2txt dpkt extract-msg fonttools hachoir javaobj `
-    lxml pyelftools pypng python-magic PyYaml minidump msoffcrypto-tool `
-    name-that-hash numpy olefile olefile oletools[full] openpyxl pandas `
-    pcode2code pcodedmp pefile peutils ppdeep pycryptodome python-magic-bin pyzipper `
-    time-decode visidata xlrd XLMMacroDeobfuscator yara-python wheel
+    chepy[extras] `
+    colorama `
+    dnslib `
+    docx2txt `
+    dpkt `
+    extract-msg `
+    fonttools `
+    hachoir `
+    lxml `
+    minidump `
+    msgpack `
+    msoffcrypto-tool `
+    name-that-hash `
+    numpy `
+    olefile `
+    oletools[full] `
+    openpyxl `
+    pandas `
+    pcode2code `
+    pcodedmp `
+    pefile `
+    peutils `
+    ppdeep `
+    pycryptodome `
+    pyelftools `
+    pypng `
+    python-magic-bin `
+    pyzipper `
+    python-magic `
+    setuptools `
+    time-decode `
+    visidata `
+    xlrd `
+    XLMMacroDeobfuscator `
+    yara-python `
+    wheel
 
 deactivate
 rm -r .wsb
