94812dc9c37ce7317ee713c6e48fc64d9b590d25
Coding for fun <peter@reuteras.net>
2023-01-31 10:47:15 +0100

    Only copy thret-intel repo to temp.

--- wsb/tools/install_python_tools.ps1
+++ wsb/tools/install_python_tools.ps1
@@ -4,6 +4,6 @@ Get-ChildItem . -Filter wheel* | Foreach-Object { python -m pip install --disabl
 Get-ChildItem . -Filter *.gz | Foreach-Object { python -m pip install --disable-pip-version-check --no-deps $_ } >> c:\temp\python.txt
 Get-ChildItem . -Filter *.whl | Foreach-Object { python -m pip install --disable-pip-version-check --no-deps $_ } >> c:\temp\python.txt
 Get-ChildItem . -Filter *.zip | Foreach-Object { python -m pip install --disable-pip-version-check --no-deps $_ } >> c:\temp\python.txt
-cp -r C:\Users\WDAGUtilityAccount\Documents\tools\downloads\git\ c:\temp
-cd C:\temp\git\threat-intel\tools\one-extract
-python -m pip install --disable-pip-version-check .
+cp -r C:\Users\WDAGUtilityAccount\Documents\tools\downloads\git\threat-intel c:\temp
+cd C:\temp\threat-intel\tools\one-extract
+python -m pip install --disable-pip-version-check . >> c:\temp\one-extract.txt
