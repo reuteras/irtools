f13ee518a561ef463ce07318260a92ecbec93b57
Coding for fun <peter@reuteras.net>
2023-02-17 09:12:00 +0100

    Covert image to jpg. Copy more info images.

--- wsb/downloadFiles.ps1
+++ wsb/downloadFiles.ps1
@@ -4,3 +4,12 @@
 .\resources\download\python.ps1
 .\resources\download\zimmerman.ps1
 cp README.md .\tools\downloads\
+cp .\resources\images\copying.png .\tools\downloads\
+cp .\resources\images\installing.png .\tools\downloads\
+cp .\resources\images\python.png .\tools\downloads\
+$sourceFile = "$PSScriptRoot\tools\downloads\sans.png"
+$saveFile = "$PSScriptRoot\tools\downloads\sans.jpg"
+Add-Type -AssemblyName system.drawing
+$imageFormat = "System.Drawing.Imaging.ImageFormat" -as [type]
+$image = [drawing.image]::FromFile($sourceFile)
+$image.Save($saveFile, $imageFormat::jpeg)
