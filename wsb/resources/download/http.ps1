24a74580132d9b7eb51602a83ab7524ae6064d98
Coding for fun <peter@reuteras.net>
2023-02-22 07:17:19 +0100

    Update sqlite and split http and GitHub releases.

--- wsb/resources/download/http.ps1
+++ wsb/resources/download/http.ps1
@@ -20,65 +20,6 @@ Function Download-File {
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 }
 
-Function Download-GitHub {
-    Param (
-        [string]$repo,
-        [string]$path
-    )
-
-    $releases = "https://api.github.com/repos/$repo/releases"
-
-    $start_time = Get-Date
-    $downloads = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets.browser_download_url
-    if ( ( Write-Output $downloads | Measure-Object -word ).Words -gt 1 ) {
-        $url = Write-Output $downloads | findstr /R "win Windows Installer.x64.exe$ qpdf cmder.7z VS_LIBEMU w64" | findstr /R /V "darwin"
-    } else {
-        $url = $downloads
-    }
-
-    if ( ( Write-Output $url | Measure-Object -word ).Words -gt 1 ) {
-        $url = Write-Output $url | findstr /R "msvc64.zip win64 Installer.x64.exe$ x86_64-pc-windows-msvc x64_win_vs17 VS_LIBEMU w64" | findstr /R /V "win32"
-    }
-
-    Write-Output "Downloading $($url)"
-    $wc = New-Object System.Net.WebClient
-    # Used to trick Sourceforge latest endpoints to get the latest x64 windows binary
-    $wc.Headers['User-Agent'] = "Wget x64"
-    Try {
-        $wc.DownloadFile($url, $path)
-    }
-    Catch {
-        throw $_
-    }
-    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-}
-
-Try {
-    Download-GitHub -repo "BurntSushi/ripgrep" -path "$($PSScriptRoot)\..\..\tools\downloads\ripgrep.zip"
-    Download-GitHub -repo "cmderdev/cmder" -path "$($PSScriptRoot)\..\..\tools\downloads\cmder.7z"
-    Download-GitHub -repo "dnSpyEx/dnSpy" -path "$($PSScriptRoot)\..\..\tools\downloads\dnSpy.zip"
-    Download-GitHub -repo "dzzie/VS_LIBEMU" -path "$($PSScriptRoot)\..\..\tools\downloads\scdbg.zip"
-    Download-GitHub -repo "hasherezade/pe-bear" -path "$($PSScriptRoot)\..\..\tools\downloads\pebear.zip"
-    Download-GitHub -repo "gchq/CyberChef" -path "$($PSScriptRoot)\..\..\tools\downloads\CyberChef.zip"
-    Download-GitHub -repo "mandiant/capa" -path "$($PSScriptRoot)\..\..\tools\downloads\capa-windows.zip"
-    Download-GitHub -repo "mandiant/flare-floss" -path "$($PSScriptRoot)\..\..\tools\downloads\floss.zip"
-    Download-GitHub -repo "mandiant/flare-fakenet-ng" -path "$($PSScriptRoot)\..\..\tools\downloads\fakenet.zip"
-    Download-GitHub -repo "mandiant/GoReSym" -path "$($PSScriptRoot)\..\..\tools\downloads\GoReSym.zip"
-    Download-GitHub -repo "NationalSecurityAgency/ghidra" -path "$($PSScriptRoot)\..\..\tools\downloads\ghidra.zip"
-    Download-GitHub -repo "Neo23x0/Loki" -path "$($PSScriptRoot)\..\..\tools\downloads\loki.zip"
-    Download-GitHub -repo "notepad-plus-plus/notepad-plus-plus" -path "$($PSScriptRoot)\..\..\tools\downloads\notepad++.exe"
-    Download-GitHub -repo "qpdf/qpdf" -path "$($PSScriptRoot)\..\..\tools\downloads\qpdf.zip"
-    Download-GitHub -repo "radareorg/radare2" -path "$($PSScriptRoot)\..\..\tools\downloads\radare2.zip"
-    Download-GitHub -repo "rizinorg/cutter" -path "$($PSScriptRoot)\..\..\tools\downloads\cutter.zip"
-    Download-GitHub -repo "stedolan/jq" -path "$($PSScriptRoot)\..\..\tools\downloads\jq.exe"
-    Download-GitHub -repo "upx/upx" -path "$($PSScriptRoot)\..\..\tools\downloads\upx.zip"
-    Download-GitHub -repo "WithSecureLabs/chainsaw" -path "$($PSScriptRoot)\..\..\tools\downloads\chainsaw.zip"
-    Download-GitHub -repo "VirusTotal/yara" -path "$($PSScriptRoot)\..\..\tools\downloads\yara.zip"
-}
-Catch {
-    $error[0] | Format-List * -force
-}
-
 Try {
     Download-File -url "https://update.code.visualstudio.com/latest/win32-x64-user/stable" -path "$($PSScriptRoot)\..\..\tools\downloads\vscode.exe"
     Download-File -url "https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml" -path "$($PSScriptRoot)\..\..\tools\downloads\sysmonconfig-export.xml"
@@ -95,7 +36,7 @@ Try {
     Download-File -url "https://www.python.org/ftp/python/3.10.10/python-3.10.10-amd64.exe" -path "$($PSScriptRoot)\..\..\tools\downloads\python3.exe"
     Download-File -url "https://npcap.com/dist/npcap-1.72.exe" -path "$($PSScriptRoot)\..\..\tools\downloads\npcap.exe"
     Download-File -url "https://1.eu.dl.wireshark.org/win64/Wireshark-win64-4.0.3.exe" -path "$($PSScriptRoot)\..\..\tools\downloads\wireshark.exe"
-    Download-File -url "https://sqlite.org/2022/sqlite-tools-win32-x86-3400100.zip" -path "$($PSScriptRoot)\..\..\tools\downloads\sqlite.zip"
+    Download-File -url "https://sqlite.org/2023/sqlite-tools-win32-x86-3410000.zip" -path "$($PSScriptRoot)\..\..\tools\downloads\sqlite.zip"
     Download-File -url "https://www.7-zip.org/a/7z2201-x64.msi" -path "$($PSScriptRoot)\..\..\tools\downloads\7zip.msi"
     Download-File -url "https://cert.at/media/files/downloads/software/densityscout/files/densityscout_build_45_windows.zip" -path "$($PSScriptRoot)\..\..\tools\downloads\DensityScout.zip"
     Download-File -url "https://nmap.org/dist/nmap-7.93-setup.exe" -path "$($PSScriptRoot)\..\..\tools\downloads\nmap.exe"
