66e816d456eb2551e689155ceaa014b35a263f80
Coding for fun <peter@reuteras.net>
2023-02-22 07:21:15 +0100

    Lint file.

--- wsb/tools/Microsoft.PowerShell_profile.ps1
+++ wsb/tools/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 C:\venv\Scripts\Activate.ps1
 
 # Use git to diff to files
-function Diff-TwoFiles ($file1, $file2) {
+function Compare-TwoFiles ($file1, $file2) {
     git diff $file1 $file2
 }
-Set-Alias gdiff Diff-TwoFiles
+Set-Alias gdiff Compare-TwoFiles
 
 # Make Windows be more like Linux :)
 Set-Alias less more
