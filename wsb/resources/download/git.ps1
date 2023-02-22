cb5a2c642f49590461b433dbf27c08dccaf4149c
Coding for fun <peter@reuteras.net>
2023-02-22 07:16:36 +0100

    Add binwalk and lint.

--- wsb/resources/download/git.ps1
+++ wsb/resources/download/git.ps1
@@ -1,24 +1,25 @@
 Write-Host "Download git repositories"
 
 New-Item -ItemType Directory -Force -Path tools\downloads\git > $null
-cd tools\downloads\git
+Set-Location tools\downloads\git
 
 $repourls = `
     "https://github.com/keydet89/Events-Ripper.git", `
     "https://github.com/Neo23x0/evt2sigma.git", `
     "https://github.com/Neo23x0/signature-base.git", `
+    "https://github.com/ReFirmLabs/binwalk.git", `
     "https://github.com/SigmaHQ/sigma.git", `
     "https://github.com/volexity/threat-intel.git"
 
 foreach ($repourl in $repourls) {
-    $repo = Write-Output $repourl | % { $_ -replace "^.*/" } | % { $_ -replace "\..*$" }
+    $repo = Write-Output $repourl | ForEach-Object { $_ -replace "^.*/" } | ForEach-Object { $_ -replace "\..*$" }
     if ( Test-Path -Path $repo ) {
-        cd $repo
+        Set-Location $repo
         git pull
-        cd ..
+        Set-Location ..
     } else {
         git clone $repourl
     }
 }
 
-cd ..\..\..
+Set-Location ..\..\..
