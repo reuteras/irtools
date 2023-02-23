d52e8061bb1f37add9e82ea0c6728ce6f860ab2b
Coding for fun <peter@reuteras.net>
2023-02-23 11:50:45 +0100

    Change path to use with new download function.
    Check version against latest.

--- wsb/resources/download/release.ps1
+++ wsb/resources/download/release.ps1
@@ -1,59 +1,51 @@
-Write-Host "Download HTTP and releases from GitHub."
+Write-Host "Download releases from GitHub."
+
+. $PSScriptRoot\common.ps1
 
 Function Get-GitHubRelease {
     Param (
         [string]$repo,
-        [string]$path
+        [string]$path,
+        [string]$match
     )
 
-    $releases = "https://api.github.com/repos/$repo/releases"
+    $releases = "https://api.github.com/repos/$repo/releases/latest"
 
-    $start_time = Get-Date
     $downloads = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets.browser_download_url
     if ( ( Write-Output $downloads | Measure-Object -word ).Words -gt 1 ) {
-        $url = Write-Output $downloads | findstr /R "win Windows Installer.x64.exe$ qpdf cmder.7z VS_LIBEMU w64" | findstr /R /V "darwin"
+        $url = Write-Output $downloads | findstr /R $match | findstr /R /V "darwin sig"
     } else {
         $url = $downloads
     }
 
-    if ( ( Write-Output $url | Measure-Object -word ).Words -gt 1 ) {
-        $url = Write-Output $url | findstr /R "msvc64.zip win64 Installer.x64.exe$ x86_64-pc-windows-msvc x64_win_vs17 VS_LIBEMU w64" | findstr /R /V "win32"
-    }
-
-    Write-Output "Downloading $($url)"
-    $wc = New-Object System.Net.WebClient
-    # Used to trick Sourceforge latest endpoints to get the latest x64 windows binary
-    $wc.Headers['User-Agent'] = "Wget x64"
-    Try {
-        $wc.DownloadFile($url, $path)
-    }
-    Catch {
-        throw $_
-    }
-    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    Write-Output "Using $url for $repo." >> .\log\log.txt
+    Get-FileFromUri -uri $url -FilePath $path
 }
 
 Try {
-    Get-GitHubRelease -repo "BurntSushi/ripgrep" -path "$($PSScriptRoot)\..\..\tools\downloads\ripgrep.zip"
-    Get-GitHubRelease -repo "cmderdev/cmder" -path "$($PSScriptRoot)\..\..\tools\downloads\cmder.7z"
-    Get-GitHubRelease -repo "dnSpyEx/dnSpy" -path "$($PSScriptRoot)\..\..\tools\downloads\dnSpy.zip"
-    Get-GitHubRelease -repo "dzzie/VS_LIBEMU" -path "$($PSScriptRoot)\..\..\tools\downloads\scdbg.zip"
-    Get-GitHubRelease -repo "hasherezade/pe-bear" -path "$($PSScriptRoot)\..\..\tools\downloads\pebear.zip"
-    Get-GitHubRelease -repo "gchq/CyberChef" -path "$($PSScriptRoot)\..\..\tools\downloads\CyberChef.zip"
-    Get-GitHubRelease -repo "mandiant/capa" -path "$($PSScriptRoot)\..\..\tools\downloads\capa-windows.zip"
-    Get-GitHubRelease -repo "mandiant/flare-floss" -path "$($PSScriptRoot)\..\..\tools\downloads\floss.zip"
-    Get-GitHubRelease -repo "mandiant/flare-fakenet-ng" -path "$($PSScriptRoot)\..\..\tools\downloads\fakenet.zip"
-    Get-GitHubRelease -repo "mandiant/GoReSym" -path "$($PSScriptRoot)\..\..\tools\downloads\GoReSym.zip"
-    Get-GitHubRelease -repo "NationalSecurityAgency/ghidra" -path "$($PSScriptRoot)\..\..\tools\downloads\ghidra.zip"
-    Get-GitHubRelease -repo "Neo23x0/Loki" -path "$($PSScriptRoot)\..\..\tools\downloads\loki.zip"
-    Get-GitHubRelease -repo "notepad-plus-plus/notepad-plus-plus" -path "$($PSScriptRoot)\..\..\tools\downloads\notepad++.exe"
-    Get-GitHubRelease -repo "qpdf/qpdf" -path "$($PSScriptRoot)\..\..\tools\downloads\qpdf.zip"
-    Get-GitHubRelease -repo "radareorg/radare2" -path "$($PSScriptRoot)\..\..\tools\downloads\radare2.zip"
-    Get-GitHubRelease -repo "rizinorg/cutter" -path "$($PSScriptRoot)\..\..\tools\downloads\cutter.zip"
-    Get-GitHubRelease -repo "stedolan/jq" -path "$($PSScriptRoot)\..\..\tools\downloads\jq.exe"
-    Get-GitHubRelease -repo "upx/upx" -path "$($PSScriptRoot)\..\..\tools\downloads\upx.zip"
-    Get-GitHubRelease -repo "WithSecureLabs/chainsaw" -path "$($PSScriptRoot)\..\..\tools\downloads\chainsaw.zip"
-    Get-GitHubRelease -repo "VirusTotal/yara" -path "$($PSScriptRoot)\..\..\tools\downloads\yara.zip"
+    Get-GitHubRelease -repo "BurntSushi/ripgrep" -path ".\tools\downloads\ripgrep.zip" -match x86_64-pc-windows-msvc
+    Get-GitHubRelease -repo "cmderdev/cmder" -path ".\tools\downloads\cmder.7z" -match cmder.7z
+    Get-GitHubRelease -repo "dnSpyEx/dnSpy" -path ".\tools\downloads\dnSpy.zip" -match win64
+    Get-GitHubRelease -repo "dzzie/pdfstreamdumper" -path ".\tools\downloads\PDFStreamDumper.exe" -match PDFStreamDumper
+    Get-GitHubRelease -repo "dzzie/VS_LIBEMU" -path ".\tools\downloads\scdbg.zip" -match VS_LIBEMU
+    Get-GitHubRelease -repo "hasherezade/pe-bear" -path ".\tools\downloads\pebear.zip" -match x64_win_vs17.zip
+    Get-GitHubRelease -repo "gchq/CyberChef" -path ".\tools\downloads\CyberChef.zip" -match CyberChef
+    Get-GitHubRelease -repo "git-for-windows/git" -path ".\tools\downloads\git.exe" -match 64-bit.exe
+    Get-GitHubRelease -repo "mandiant/capa" -path ".\tools\downloads\capa-windows.zip" -match windows
+    Get-GitHubRelease -repo "mandiant/flare-floss" -path ".\tools\downloads\floss.zip" -match windows
+    Get-GitHubRelease -repo "mandiant/flare-fakenet-ng" -path ".\tools\downloads\fakenet.zip" -match fakenet
+    Get-GitHubRelease -repo "mandiant/GoReSym" -path ".\tools\downloads\GoReSym.zip" -match GoReSym
+    Get-GitHubRelease -repo "NationalSecurityAgency/ghidra" -path ".\tools\downloads\ghidra.zip" -match ghidra
+    Get-GitHubRelease -repo "Neo23x0/Loki" -path ".\tools\downloads\loki.zip" -match loki
+    Get-GitHubRelease -repo "notepad-plus-plus/notepad-plus-plus" -path ".\tools\downloads\notepad++.exe" -match Installer.x64.exe
+    Get-GitHubRelease -repo "qpdf/qpdf" -path ".\tools\downloads\qpdf.zip" -match msvc64.zip
+    Get-GitHubRelease -repo "radareorg/radare2" -path ".\tools\downloads\radare2.zip" -match w64.zip
+    Get-GitHubRelease -repo "rizinorg/cutter" -path ".\tools\downloads\cutter.zip" -match Windows-x86_64.zip
+    Get-GitHubRelease -repo "stedolan/jq" -path ".\tools\downloads\jq.exe" -match win64
+    Get-GitHubRelease -repo "thumbcacheviewer/thumbcacheviewer" -path ".\tools\downloads\thumbcacheviewer.zip" -match viewer_64
+    Get-GitHubRelease -repo "upx/upx" -path ".\tools\downloads\upx.zip" -match win64
+    Get-GitHubRelease -repo "WithSecureLabs/chainsaw" -path ".\tools\downloads\chainsaw.zip" -match x86_64-pc-windows-msvc
+    Get-GitHubRelease -repo "VirusTotal/yara" -path ".\tools\downloads\yara.zip" -match win64
 }
 Catch {
     $error[0] | Format-List * -force
