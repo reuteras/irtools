c9bb492eea296f5e643c61c00da4c0d90b5a5353
Coding for fun <peter@reuteras.net>
2023-02-24 12:50:49 +0100

    Add alias for grep, tail and tailf

--- wsb/tools/Microsoft.PowerShell_profile.ps1
+++ wsb/tools/Microsoft.PowerShell_profile.ps1
@@ -9,5 +9,11 @@ function Compare-TwoFiles ($file1, $file2) {
 }
 Set-Alias gdiff Compare-TwoFiles
 
+function tailf {
+    Get-Content -Wait $args
+}
+
 # Make Windows be more like Linux :)
 Set-Alias less more
+Set-Alias grep findstr
+Set-Alias tail Get-Content
